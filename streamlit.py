a2e55b440d32fc6d77d0954c0bc5a0e0c05a8237
heroku_deployment <18it089@charusat.edu.in>
2021-10-11 15:25:58 +0530

    initial commit

---
+++ streamlit.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import tensorflow
+from PIL import Image, ImageOps
+from tensorflow.keras.models import load_model
+
+from tensorflow import keras 
+import numpy as np
+
+st.title("Image Classification with Google's Teachable Machine")
+st.header("Brain Tumor MRI Classification Example")
+st.text("Upload a brain MRI Image for image classification as tumor or no-tumor")
+st.text(tensorflow.__version__)
+def teachable_machine_classification(img):
+    # Load the model
+    model = tensorflow.keras.models.load_model('Model-RESNET-final.h5')
+
+    # Create the array of the right shape to feed into the keras model
+    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+    image = img
+    #image sizing
+    size = (224, 224)
+    image = ImageOps.fit(image, size, Image.ANTIALIAS)
+
+    #turn the image into a numpy array
+    image_array = np.asarray(image)
+    # Normalize the image
+    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+
+    # Load the image into the array
+    data[0] = normalized_image_array
+
+    # run the inference
+    prediction = model.predict(data)
+    return (prediction) # return position of the highest probability
+
+
+uploaded_file = st.file_uploader("Choose a brain MRI ...", type="jpg")
+if uploaded_file is not None:
+        image = Image.open(uploaded_file)
+        st.image(image, caption='Uploaded MRI.', use_column_width=True)
+        st.write("")
+        st.write("Classifying...")
+        label = teachable_machine_classification(image)
+        st.write(label)
+
