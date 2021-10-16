813763b9c9604ce424a814353ee4d1136aefc25a
KaranPatel20 <54732377+KaranPatel20@users.noreply.github.com>
2021-10-16 19:50:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import tensorflow
 from PIL import Image, ImageOps
 from tensorflow.keras.models import load_model
-from model import teachable_machine_classification
+from image_model_load import teachable_machine_classification
 from tensorflow import keras 
 import numpy as np
 
