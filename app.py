bf7a82755ee3768ddbf473729241abdaa9aad646
KaranPatel20 <54732377+KaranPatel20@users.noreply.github.com>
2021-10-16 19:28:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import tensorflow
 from PIL import Image, ImageOps
 from tensorflow.keras.models import load_model
-
+from model import teachable_machine_classification
 from tensorflow import keras 
 import numpy as np
 
@@ -10,29 +10,6 @@ import numpy as np
 st.header("Brain Tumor MRI Classification Example")
 st.text("Upload a brain MRI Image for image classification as tumor or no-tumor")
 st.text(tensorflow.__version__)
-def teachable_machine_classification(img):
-    # Load the model
-    model = tensorflow.keras.models.load_model('Model-RESNET-final.h5')
-
-    # Create the array of the right shape to feed into the keras model
-    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-    image = img
-    #image sizing
-    size = (224, 224)
-    image = ImageOps.fit(image, size, Image.ANTIALIAS)
-
-    #turn the image into a numpy array
-    image_array = np.asarray(image)
-    # Normalize the image
-    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
-
-    # Load the image into the array
-    data[0] = normalized_image_array
-
-    # run the inference
-    prediction = model.predict(data)
-    return (prediction) # return position of the highest probability
-
 
 uploaded_file = st.file_uploader("Choose a brain MRI ...", type="jpg")
 if uploaded_file is not None:
